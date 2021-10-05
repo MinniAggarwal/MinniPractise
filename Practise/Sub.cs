514712aab4a7a6e04f4c785aab6eb83d5a4f5ef0
minnia <minnia@cybage.com>
2021-10-05 14:40:28 +0530

    committoggggbranch

---
+++ Practise/Sub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practise
+{
+    class Sub
+    {
+        public int Substraction(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
